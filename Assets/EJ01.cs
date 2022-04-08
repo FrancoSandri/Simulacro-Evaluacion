1e91c219251d30f39a486f5252bdcefcfe6d181d
FrancoSandri <46960579@est.ort.edu.ar>
2022-04-08 11:14:19 -0300

    EJ01 done

---
+++ Assets/EJ01.cs
@@ -0,0 +1,29 @@
+﻿/*1.Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ01 : MonoBehaviour
+{
+    public int Numero1;
+    public int Numero2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Numero1 == Numero2)
+        {
+            Debug.Log("Los numeros ingresados son iguales");
+        }
+        else
+        {
+            Debug.Log("Los numeros ingresados no son iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
